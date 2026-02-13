73cfde3e52157c4a9eb3b8cb8a944a012496c1ad
Ramiz <ramiz.maksumic@gmail.com>
2026-02-13 12:07:32 +0100

    Izmjena putanje fontova.

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="n0K3asBJjJj2Qb0tg4g5JRTRXN0msBhviA_LWn26gbs" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="Reunion web & marketing agencija se bavi dizajnom i izradom web stranica, web aplikacija, kao i digitalnlim marketingom. Vezane usluge su oglašavanje (Google ads, Facebook & Instagram ads), kao i profesionalna fotografija.">
     <link href="/assets/fonts/fontawesome/css/all.min.css" rel="stylesheet">
