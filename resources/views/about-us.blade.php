201433aec23b81c7508eefb9c53c3c6b80c6c4b2
Ramiz <ramiz.maksumic@gmail.com>
2026-02-20 11:46:01 +0100

    Dodate slike u O nama sekciji.

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -7,7 +7,7 @@
 <section class=" bg-secondary">
 
     <div class="flex flex-col mx-auto px-5 md:px-16 max-w-screen-2xl justify-center items-center py-10 md:py-20">
-        <p class="text-center">
+        <p class="text-center font-body">
             Reunion – marketing i web agencija je tim koji pomaže brendovima da izgrade snažno online prisustvo kroz moderan web dizajn, kvalitetan razvoj web stranica i strateški digitalni marketing. Naš fokus je na rješenjima koja donose konkretne rezultate – više upita, bolju prezentaciju i jasniju komunikaciju s klijentima. Kroz dugogodišnje iskustvo i rad s različitim industrijama, posebno u turizmu, pristupamo svakom projektu sistematično, profesionalno i s punom pažnjom na detalje, kako bismo isporučili web koji izgleda vrhunski i radi još bolje.
         </p>
         <a href="/kontakt"
@@ -32,7 +32,7 @@
     <div class="flex flex-col mx-auto px-5 md:px-16 max-w-screen-2xl justify-between py-10 md:py-10">
         <h2 class="font-heading text-5xl text-center">Zašto baš mi?</h2>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-3 gap-x-5 mt-16">
-            <div class="mt-5"><x-about-card title="10 + godina s nama" text="Više od 10 godina gradimo digitalna rješenja koja pomažu firmama da rastu i budu prepoznatljive na tržištu. Kroz iskustvo na desetinama projekata naučili smo šta zaista donosi rezultate, a šta je samo trend. Upravo zato nudimo provjeren pristup, realne rokove i podršku na koju se možete osloniti – prije, tokom i nakon izrade web stranice." icon="fa regular fa-calendar-days" /></div>
+            <div class="mt-5"><x-about-card title="Više od 10 godina iskustva" text="Više od 10 godina gradimo digitalna rješenja koja pomažu firmama da rastu i budu prepoznatljive na tržištu. Kroz iskustvo na desetinama projekata naučili smo šta zaista donosi rezultate, a šta je samo trend. Upravo zato nudimo provjeren pristup, realne rokove i podršku na koju se možete osloniti – prije, tokom i nakon izrade web stranice." icon="fa regular fa-calendar-days" /></div>
             <div class="mt-5"><x-about-card title="Rezultati, ne pretpostavke" text="Kod nas odluke ne nastaju “po osjećaju”, nego na osnovu onoga što donosi stvarne rezultate. Svaki web i svaka kampanja imaju jasan cilj – više upita, bolju prodaju ili jaču vidljivost – a naš fokus je na mjerljivim pokazateljima i konkretnim poboljšanjima. Testiramo, analiziramo i optimizujemo, kako biste dobili rješenje koje radi u praksi, a ne samo lijepo izgleda." icon="fa solid fa-square-poll-vertical" /></div>
             <div class="mt-5"><x-about-card title="Direktna komunikacija" text="Kod nas nema komplikacija i “prebacivanja” s osobe na osobu – imate direktan kontakt s timom koji radi na Vašem projektu. Jasno dogovaramo korake, rokove i prioritete, te Vas redovno informišemo o napretku. Na taj način štedimo vrijeme, izbjegavamo nesporazume i osiguravamo da dobijete rješenje koje tačno odgovara Vašim ciljevima." icon="fa-regular fa-message" /></div>
             <div class="mt-5"><x-about-card title="Kreativnost & tehnička preciznost" text="Vjerujemo da vrhunski web nastaje tek kada se spoje dobar dizajn i besprijekorna izvedba. Zato gradimo stranice koje vizuelno privlače pažnju, ali su istovremeno brze, stabilne, sigurne i funkcionalne na svim uređajima. Svaki detalj – od tipografije i strukture sadržaja do optimizacije i performansi – radimo precizno, kako bi krajnji rezultat bio i lijep i učinkovit." icon="fa-solid fa-gear" /></div>
@@ -131,7 +131,7 @@
             <x-team-member
                 name="Ramiz Maksumic"
                 position="Web Development & Desgin Lead"
-                image="{{ asset('images/team/default.jpg') }}" />
+                image="{{ asset('images/team/Ramiz.jpg') }}" />
 
             <x-team-member
                 name="Irma Kajan"
